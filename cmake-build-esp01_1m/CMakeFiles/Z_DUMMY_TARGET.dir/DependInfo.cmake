c06c8e18eeda31fa4569a32586d42e4bdd4e65c4
JHostalek <jan.hostalek@post.cz>
2020-08-05 09:22:49 +0200

    8266 upload

---
+++ cmake-build-esp01_1m/CMakeFiles/Z_DUMMY_TARGET.dir/DependInfo.cmake
@@ -0,0 +1,65 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/ownCloud/other/BEEing PlatformIO/esp8266/src/main.cpp" "D:/ownCloud/other/BEEing PlatformIO/esp8266/cmake-build-esp01_1m/CMakeFiles/Z_DUMMY_TARGET.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/tools/sdk/include"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/tools/sdk/libc/xtensa-lx106-elf/include"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/cores/esp8266"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/tools/sdk/lwip2/include"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/variants/generic"
+  "C:/Users/janho/.platformio/lib/Adafruit BME280 Library"
+  "C:/Users/janho/.platformio/lib/Adafruit BMP280 Library"
+  "C:/Users/janho/.platformio/lib/Adafruit Unified Sensor"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/ArduinoOTA"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/DNSServer/src"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/EEPROM"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266AVRISP/src"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266HTTPClient/src"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266HTTPUpdateServer/src"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266LLMNR"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266NetBIOS"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266SSDP"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266SdFat/src"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266WebServer/src"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266WiFi/src"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266WiFiMesh/src"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266httpUpdate/src"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266mDNS/src"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/Ethernet/src"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/GDBStub/src"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/Hash/src"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/LittleFS/src"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/SD/src"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/SDFS/src"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/SPI"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/SPISlave/src"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/Servo/src"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/SoftwareSerial/src"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/TFT_Touch_Shield_V2"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/Ticker/src"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/Wire"
+  "C:/Users/janho/.platformio/packages/framework-arduinoespressif8266/libraries/esp8266/src"
+  "C:/Users/janho/.platformio/packages/toolchain-xtensa/xtensa-lx106-elf/include/c++/4.8.2"
+  "C:/Users/janho/.platformio/packages/toolchain-xtensa/xtensa-lx106-elf/include/c++/4.8.2/xtensa-lx106-elf"
+  "C:/Users/janho/.platformio/packages/toolchain-xtensa/lib/gcc/xtensa-lx106-elf/4.8.2/include"
+  "C:/Users/janho/.platformio/packages/toolchain-xtensa/lib/gcc/xtensa-lx106-elf/4.8.2/include-fixed"
+  "C:/Users/janho/.platformio/packages/toolchain-xtensa/xtensa-lx106-elf/include"
+  "C:/Users/janho/.platformio/packages/tool-unity"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
