86b2d89b472813caf5aded3bd228d12c74fee9fb
JHostalek <jan.hostalek@post.cz>
2020-08-06 21:10:36 +0200

    before test esp-wifi at the same time

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,28 @@
 #include <ThingSpeak.h>
 
 unsigned long myChannelNumber = 829059;
-const char* myWriteAPIKey = "4OL312MW06WZDDI3";
+unsigned long myChannelNumber2 = 1112090;
+const char *myWriteAPIKey = "4OL312MW06WZDDI3";
+const char *myWriteAPIKey2 = "JVZXT5M3X80ZB87D";
 //const char* ssid = ":("; // the ssid/name of the wifi, the esp will be connected to
 //const char* password = "soulknight"; // the password of that wifi
 //WiFiClient client;
 
 // Replace with your network credentials (STATION)
-const char* ssidStation = ":(";
-const char* passwordStation = "soulknight";
-
+//const char *ssidStation = "HWAP";
+//const char *passwordStation = "soulknight";
+const char *ssidStation = "semtamtuk.net";
+const char *passwordStation = "PzgKCTvjC96bZw9xEw9E";
+WiFiClient client;
 // ACCESS POINT credentials
-const char* ssidAP = ":)";
-const char* passwordAP = "soulknight";
+const char *ssidAP = ":)";
+const char *passwordAP = "soulknight";
 
-#define CHANNEL 1
+int CHANNEL = 1;
 
 long long oldTime = millis();
+bool switcher = true;
+bool dataReceived = false;
 
 //#define CHANNEL 1
 
@@ -36,13 +42,13 @@ typedef struct DataStruct {
 } DataStruct __attribute__((packed));
 DataStruct dataStruct;
 
+
 // Init ESP Now with fallback
 void InitESPNow() {
     WiFi.disconnect();
     if (esp_now_init() == ESP_OK) {
         Serial.println("ESPNow Init Success");
-    }
-    else {
+    } else {
         Serial.println("ESPNow Init Failed");
         // Retry InitESPNow, add a counte and then restart?
         // InitESPNow();
@@ -82,12 +88,14 @@ void printAll() {
     Serial.print(" | BATTERY: ");
     Serial.println(dataStruct.batt);
 }
+
 // callback when data is recv from Master
 void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
     char macStr[18];
     snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
              mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-    Serial.print("Last Packet Recv from: "); Serial.println(macStr);
+    Serial.print("Last Packet Recv from: ");
+    Serial.println(macStr);
     //Serial.print("Last Packet Recv Data: "); Serial.println(*data);
     DataStruct incdata;
     memcpy(&incdata, data, sizeof(incdata));
@@ -101,52 +109,106 @@ void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
     dataStruct.gz = incdata.gz;
     dataStruct.batt = incdata.batt;
     printAll();
-    Serial.println("");
-    if (Serial2.available()){
-        Serial.println("Sending data via serial...");
-        const char* dp = (const char*) &dataStruct;
-        for (int i = 0; i < sizeof(dataStruct); i++) Serial2.write(*dp++);
-    }
+//    Serial.println("");
+//    if (Serial2.available()){
+//        Serial.println("Sending data via serial...");
+//        const char* dp = (const char*) &dataStruct;
+//        for (int i = 0; i < sizeof(dataStruct); i++) Serial2.write(*dp++);
+//    }
+
     Serial.println("-----DONE-----");
+    esp_now_unregister_recv_cb();
+    esp_now_deinit();
+    switcher = true;
 }
 
 
 void setup() {
-
-
-//    WiFi.begin(ssid, password);
-//    long long oldTime = millis();
-//    Serial.println("Connecting to wifi...");
-//    while (WiFi.status() != WL_CONNECTED) {
-//        if(millis()-oldTime > 10000){\
-//            Serial.println("Connecting FAILED!... RESTARTING");
-//            ESP.restart();
-//        }
-//    }
-//    Serial.println("WiFi connected");
-//    Serial.println("IP address: ");
-//    Serial.println(WiFi.localIP());
-//
-//    //ThingSpeak.begin(client);
-
     Serial.begin(115200);
-    Serial2.begin(115200);
+    //Serial2.begin(115200);
     Serial.println("THIS DEVICE IS SERVER - ESPNow");
-    //Set device in AP mode to begin with
-    WiFi.mode(WIFI_AP);
-    // configure device AP mode
-    configDeviceAP();
-    // This is the mac address of the Slave in AP Mode
-    Serial.print("AP MAC: "); Serial.println(WiFi.softAPmacAddress());
-    // Init ESPNow with a fallback logic
-    InitESPNow();
-    // Once ESPNow is successfully Init, we will register for recv CB to
-    // get recv packer info.
-    esp_now_register_recv_cb(OnDataRecv);
-
-
+    pinMode(BUILTIN_LED, OUTPUT);
 }
 
 void loop() {
+    if (millis() - oldTime > 10000) {
+        oldTime = millis();
+        if (switcher) {
+            retry:
+            WiFi.disconnect();
+            Serial.println("Connecting to WiFi..");
+            WiFi.mode(WIFI_STA);
+            WiFi.begin(ssidStation, passwordStation);
+            long long oldTimeTemp = millis();
+            while (WiFi.status() != WL_CONNECTED) {
+                if (millis() - oldTimeTemp > 30000) {
+                    Serial.println("FAILED! ... RESTARTING");
+                    WiFi.disconnect();
+                    delay(500);
+                    goto retry;
+                    //ESP.restart();
+                }
+            }
+            Serial.println(WiFi.channel());
+            Serial.println("Successfully connected");
+            digitalWrite(BUILTIN_LED, HIGH);
+            ThingSpeak.begin(client);
+            if (dataStruct.Tmp1 != 0 && dataStruct.Audio != 0) {
+                ThingSpeak.writeField(myChannelNumber, 1, dataStruct.Tmp1, myWriteAPIKey);
+                Serial.print("send to ch1 f1:");
+                Serial.println(dataStruct.Tmp1);
+                delay(50000);
+                ThingSpeak.writeField(myChannelNumber, 2, dataStruct.Audio, myWriteAPIKey);
+                Serial.print("send to ch1 f2:");
+                Serial.println(dataStruct.Audio);
+                delay(50000);
+                ThingSpeak.writeField(myChannelNumber, 3, dataStruct.ax, myWriteAPIKey);
+                Serial.print("send to ch1 f3:");
+                Serial.println(dataStruct.ax);
+                delay(50000);
+                ThingSpeak.writeField(myChannelNumber, 4, dataStruct.ay, myWriteAPIKey);
+                Serial.print("send to ch1 f4:");
+                Serial.println(dataStruct.ay);
+                delay(50000);
+                ThingSpeak.writeField(myChannelNumber, 5, dataStruct.az, myWriteAPIKey);
+                Serial.print("send to ch1 f5:");
+                Serial.println(dataStruct.az);
+                delay(50000);
+                ThingSpeak.writeField(myChannelNumber, 6, dataStruct.gx, myWriteAPIKey);
+                Serial.print("send to ch1 f6:");
+                Serial.println(dataStruct.gx);
+                delay(50000);
+                ThingSpeak.writeField(myChannelNumber, 7, dataStruct.gy, myWriteAPIKey);
+                Serial.print("send to ch1 f7:");
+                Serial.println(dataStruct.gy);
+                delay(50000);
+                ThingSpeak.writeField(myChannelNumber, 8, dataStruct.gz, myWriteAPIKey);
+                Serial.print("send to ch1 f8:");
+                Serial.println(dataStruct.gz);
+                delay(50000);
+                ThingSpeak.writeField(myChannelNumber2, 1, dataStruct.batt, myWriteAPIKey2);
+                Serial.print("send to ch1 f8:");
+                Serial.println(dataStruct.batt);
+                delay(50000);
+            }
+            switcher = false;
+            WiFi.disconnect();
+            digitalWrite(BUILTIN_LED, LOW);
+        } else {
+            Serial.println("Setting up ESPNow...");
+            //Set device in AP mode to begin with
+            WiFi.mode(WIFI_AP);
+            // configure device AP mode
+            configDeviceAP();
+            // This is the mac address of the Slave in AP Mode
+            Serial.print("AP MAC: ");
+            Serial.println(WiFi.softAPmacAddress());
+            // Init ESPNow with a fallback logic
+            InitESPNow();
+            // Once ESPNow is successfully Init, we will register for recv CB to
+            // get recv packer info.
+            esp_now_register_recv_cb(OnDataRecv);
+        }
+    }
 
 }
