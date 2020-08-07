4475ce8a0eb59be3c6fe99ceaecb850a6bba344a
JHostalek <jan.hostalek@post.cz>
2020-08-07 14:32:41 +0200

    esp-now, wifi probably working together

--- src/main.cpp
+++ src/main.cpp
@@ -109,100 +109,141 @@ void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
     dataStruct.gz = incdata.gz;
     dataStruct.batt = incdata.batt;
     printAll();
+//    Serial.println("");
+//    if (Serial2.available()){
+//        Serial.println("Sending data via serial...");
+//        const char* dp = (const char*) &dataStruct;
+//        for (int i = 0; i < sizeof(dataStruct); i++) Serial2.write(*dp++);
+//    }
 
     Serial.println("-----DONE-----");
-    esp_now_unregister_recv_cb();
-    esp_now_deinit();
-    switcher = true;
+//    esp_now_unregister_recv_cb();
+//    esp_now_deinit();
+//    switcher = true;
 }
 
+void WiFiReset() {
+    WiFi.persistent(false);
+    WiFi.disconnect();
+    WiFi.mode(WIFI_OFF);
+}
 
 void setup() {
     Serial.begin(115200);
     //Serial2.begin(115200);
     Serial.println("THIS DEVICE IS SERVER - ESPNow");
     pinMode(BUILTIN_LED, OUTPUT);
+    WiFiReset();
+    WiFi.mode(WIFI_AP_STA);
+    WiFi.begin(ssidStation, passwordStation, CHANNEL,0,1);
+    long long oldTimeTemp = millis();
+    while (WiFi.status() != WL_CONNECTED) {
+        if (millis() - oldTimeTemp > 30000) {
+            Serial.println("FAILED! ... RESTARTING");
+            WiFi.disconnect();
+            delay(500);
+            ESP.restart();
+        }
+    }
+    Serial.println(WiFi.channel());
+    Serial.println("Successfully connected");
+
+    Serial.println("Setting up ESPNow...");
+    //Set device in AP mode to begin with
+    // configure device AP mode
+    configDeviceAP();
+    // This is the mac address of the Slave in AP Mode
+    Serial.print("AP MAC: ");
+    Serial.println(WiFi.softAPmacAddress());
+    // Init ESPNow with a fallback logic
+    InitESPNow();
+    // Once ESPNow is successfully Init, we will register for recv CB to
+    // get recv packer info.
+    esp_now_register_recv_cb(OnDataRecv);
+
+
+
 }
 
 void loop() {
-    if (millis() - oldTime > 10000) {
-        oldTime = millis();
-        if (switcher) {
-            retry:
-            WiFi.disconnect();
-            Serial.println("Connecting to WiFi..");
-            WiFi.mode(WIFI_STA);
-            WiFi.begin(ssidStation, passwordStation);
-            long long oldTimeTemp = millis();
-            while (WiFi.status() != WL_CONNECTED) {
-                if (millis() - oldTimeTemp > 30000) {
-                    Serial.println("FAILED! ... RESTARTING");
-                    WiFi.disconnect();
-                    delay(500);
-                    goto retry;
-                    //ESP.restart();
-                }
-            }
-            Serial.println(WiFi.channel());
-            Serial.println("Successfully connected");
-            digitalWrite(BUILTIN_LED, HIGH);
-            ThingSpeak.begin(client);
-            if (dataStruct.Tmp1 != 0 && dataStruct.Audio != 0) {
-                ThingSpeak.writeField(myChannelNumber, 1, dataStruct.Tmp1, myWriteAPIKey);
-                Serial.print("send to ch1 f1:");
-                Serial.println(dataStruct.Tmp1);
-                delay(50000);
-                ThingSpeak.writeField(myChannelNumber, 2, dataStruct.Audio, myWriteAPIKey);
-                Serial.print("send to ch1 f2:");
-                Serial.println(dataStruct.Audio);
-                delay(50000);
-                ThingSpeak.writeField(myChannelNumber, 3, dataStruct.ax, myWriteAPIKey);
-                Serial.print("send to ch1 f3:");
-                Serial.println(dataStruct.ax);
-                delay(50000);
-                ThingSpeak.writeField(myChannelNumber, 4, dataStruct.ay, myWriteAPIKey);
-                Serial.print("send to ch1 f4:");
-                Serial.println(dataStruct.ay);
-                delay(50000);
-                ThingSpeak.writeField(myChannelNumber, 5, dataStruct.az, myWriteAPIKey);
-                Serial.print("send to ch1 f5:");
-                Serial.println(dataStruct.az);
-                delay(50000);
-                ThingSpeak.writeField(myChannelNumber, 6, dataStruct.gx, myWriteAPIKey);
-                Serial.print("send to ch1 f6:");
-                Serial.println(dataStruct.gx);
-                delay(50000);
-                ThingSpeak.writeField(myChannelNumber, 7, dataStruct.gy, myWriteAPIKey);
-                Serial.print("send to ch1 f7:");
-                Serial.println(dataStruct.gy);
-                delay(50000);
-                ThingSpeak.writeField(myChannelNumber, 8, dataStruct.gz, myWriteAPIKey);
-                Serial.print("send to ch1 f8:");
-                Serial.println(dataStruct.gz);
-                delay(50000);
-                ThingSpeak.writeField(myChannelNumber2, 1, dataStruct.batt, myWriteAPIKey2);
-                Serial.print("send to ch1 f8:");
-                Serial.println(dataStruct.batt);
-                delay(50000);
-            }
-            switcher = false;
-            WiFi.disconnect();
-            digitalWrite(BUILTIN_LED, LOW);
-        } else {
-            Serial.println("Setting up ESPNow...");
-            //Set device in AP mode to begin with
-            WiFi.mode(WIFI_AP);
-            // configure device AP mode
-            configDeviceAP();
-            // This is the mac address of the Slave in AP Mode
-            Serial.print("AP MAC: ");
-            Serial.println(WiFi.softAPmacAddress());
-            // Init ESPNow with a fallback logic
-            InitESPNow();
-            // Once ESPNow is successfully Init, we will register for recv CB to
-            // get recv packer info.
-            esp_now_register_recv_cb(OnDataRecv);
-        }
-    }
+//    if (millis() - oldTime > 10000) {
+//        oldTime = millis();
+//        if (switcher) {
+//            retry:
+//            WiFi.disconnect();
+//            Serial.println("Connecting to WiFi..");
+//            WiFi.mode(WIFI_STA);
+//            WiFi.begin(ssidStation, passwordStation);
+//            long long oldTimeTemp = millis();
+//            while (WiFi.status() != WL_CONNECTED) {
+//                if (millis() - oldTimeTemp > 30000) {
+//                    Serial.println("FAILED! ... RESTARTING");
+//                    WiFi.disconnect();
+//                    delay(500);
+//                    goto retry;
+//                    //ESP.restart();
+//                }
+//            }
+//            Serial.println(WiFi.channel());
+//            Serial.println("Successfully connected");
+//            digitalWrite(BUILTIN_LED, HIGH);
+//            ThingSpeak.begin(client);
+//            if (dataStruct.Tmp1 != 0 && dataStruct.Audio != 0) {
+//                ThingSpeak.writeField(myChannelNumber, 1, dataStruct.Tmp1, myWriteAPIKey);
+//                Serial.print("send to ch1 f1:");
+//                Serial.println(dataStruct.Tmp1);
+//                delay(50000);
+//                ThingSpeak.writeField(myChannelNumber, 2, dataStruct.Audio, myWriteAPIKey);
+//                Serial.print("send to ch1 f2:");
+//                Serial.println(dataStruct.Audio);
+//                delay(50000);
+//                ThingSpeak.writeField(myChannelNumber, 3, dataStruct.ax, myWriteAPIKey);
+//                Serial.print("send to ch1 f3:");
+//                Serial.println(dataStruct.ax);
+//                delay(50000);
+//                ThingSpeak.writeField(myChannelNumber, 4, dataStruct.ay, myWriteAPIKey);
+//                Serial.print("send to ch1 f4:");
+//                Serial.println(dataStruct.ay);
+//                delay(50000);
+//                ThingSpeak.writeField(myChannelNumber, 5, dataStruct.az, myWriteAPIKey);
+//                Serial.print("send to ch1 f5:");
+//                Serial.println(dataStruct.az);
+//                delay(50000);
+//                ThingSpeak.writeField(myChannelNumber, 6, dataStruct.gx, myWriteAPIKey);
+//                Serial.print("send to ch1 f6:");
+//                Serial.println(dataStruct.gx);
+//                delay(50000);
+//                ThingSpeak.writeField(myChannelNumber, 7, dataStruct.gy, myWriteAPIKey);
+//                Serial.print("send to ch1 f7:");
+//                Serial.println(dataStruct.gy);
+//                delay(50000);
+//                ThingSpeak.writeField(myChannelNumber, 8, dataStruct.gz, myWriteAPIKey);
+//                Serial.print("send to ch1 f8:");
+//                Serial.println(dataStruct.gz);
+//                delay(50000);
+//                ThingSpeak.writeField(myChannelNumber2, 1, dataStruct.batt, myWriteAPIKey2);
+//                Serial.print("send to ch1 f8:");
+//                Serial.println(dataStruct.batt);
+//                delay(50000);
+//            }
+//            switcher = false;
+//            WiFi.disconnect();
+//            digitalWrite(BUILTIN_LED, LOW);
+//        } else {
+//            Serial.println("Setting up ESPNow...");
+//            //Set device in AP mode to begin with
+//            WiFi.mode(WIFI_AP);
+//            // configure device AP mode
+//            configDeviceAP();
+//            // This is the mac address of the Slave in AP Mode
+//            Serial.print("AP MAC: ");
+//            Serial.println(WiFi.softAPmacAddress());
+//            // Init ESPNow with a fallback logic
+//            InitESPNow();
+//            // Once ESPNow is successfully Init, we will register for recv CB to
+//            // get recv packer info.
+//            esp_now_register_recv_cb(OnDataRecv);
+//        }
+//    }
 
 }
